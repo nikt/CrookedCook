a824b8deefd124db7bd3bae6eb7005b6536de288
Nicholas Thomson <nwhthoms@uwaterloo.ca>
2018-01-22 23:21:39 -0500

    Playing with basic layout stuff

--- CrookedCook/IngredientFactory.swift
+++ CrookedCook/IngredientFactory.swift
@@ -30,14 +30,29 @@ class IngredientFactory {
             ingredient.name = name
             ingredient.price = dictionary["price"] as? Double
             
+            // set up kitchen sprite
+            let kitchenSprite = SKSpriteNode(imageNamed: "bucket")
+            kitchenSprite.colorBlendFactor = 1.0
+            
+            // set up recipe sprite
+            let recipeSprite = SKSpriteNode(imageNamed: "bucket")
+            recipeSprite.colorBlendFactor = 1.0
+            
             if let colorComponents = dictionary["color"] as? NSArray {
                 ingredient.color = UIColor(
-                    red: colorComponents[0] as! CGFloat,
-                    green: colorComponents[1] as! CGFloat,
-                    blue: colorComponents[2] as! CGFloat,
-                    alpha: 1.0
+                    red:    (colorComponents[0] as! CGFloat) / 255.0,
+                    green:  (colorComponents[1] as! CGFloat) / 255.0,
+                    blue:   (colorComponents[2] as! CGFloat) / 255.0,
+                    alpha:  1.0
                 )
+                
+                kitchenSprite.color = ingredient.color!
+                recipeSprite.color = ingredient.color!
             }
+            
+            // final assignments
+            ingredient.kitchenSprite = kitchenSprite
+            ingredient.recipeSprite = recipeSprite
             
             return ingredient;
         }
--- CrookedCook/ViewController.swift
+++ CrookedCook/ViewController.swift
@@ -7,16 +7,61 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    var bucketSprite : SKSpriteNode?
+    var gameScene : SKScene?
+    
+    override func loadView() {
+        self.view = SKView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.backgroundColor = UIColor.blue
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        NSLog("test")
+        gameScene = SKScene(size: self.view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        gameScene?.scaleMode = .resizeFill
+        
+        let ingredientArray:[String] = ["Lettuce", "Bacon", "BBQ Sauce", "Bun"]
+        //        let rowCount = 2    // max number of ingredients per row
+        var i = 0
+        let interval = (gameScene?.size.width)! / 5.0
+        
+        for ingredientName in (ingredientArray as NSArray as! [String]) {
+            if let ingredient = IngredientFactory.getIngredient(name: ingredientName) {
+                let sprite = ingredient.kitchenSprite
+                
+                let position = CGPoint(x: interval * (CGFloat)(i + 1), y: (gameScene?.size.height)! / 2.0)
+                
+                NSLog("position (%f, %f)", position.x, position.y)
+                
+                sprite?.position = position
+                gameScene?.addChild(sprite!)
+            }
+            
+            i = i + 1
+        }
+        
+        gameScene?.backgroundColor = UIColor.blue
+        
+        skView.presentScene(gameScene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
